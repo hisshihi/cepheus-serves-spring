e14161646ab25d46394eab1513ab1ee0c3acff76
HissDev <hissoffc@gmail.com>
2024-01-31 18:45:33 +0300

    Добавил конфигурации для защиты приложения. Добавил метод для получения данных пользователя и закодировании пароля. Также метод для кодировании пароля и менеджер аунтификации с стандартными настройками.

---
+++ src/main/java/com/example/web/cepheusservice/config/ApplicationConfig.java
@@ -0,0 +1,51 @@
+package com.example.web.cepheusservice.config;
+
+import com.example.web.cepheusservice.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+    }
+
+//    Получение доступа к данным пользователя и закодировании пароля и тд
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        Пользователь
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+//        Закодирование пароля
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+//    Создаётся менеджер аунтификации
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
