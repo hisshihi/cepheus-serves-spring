7ef10bcdf1d7b78e660110cfb0983c10cc7f17ce
HissDev <hissoffc@gmail.com>
2024-02-02 22:29:14 +0300

    Добавил поиск пользователя по токену и поиск всех пользователей.

--- src/main/java/com/example/web/cepheusservice/controllers/UserController.java
+++ src/main/java/com/example/web/cepheusservice/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.example.web.cepheusservice.controllers;
 
+import com.example.web.cepheusservice.auth.AuthenticationRequest;
+import com.example.web.cepheusservice.auth.AuthenticationResponse;
 import com.example.web.cepheusservice.domain.dto.ProductDto;
 import com.example.web.cepheusservice.domain.dto.UserDto;
 import com.example.web.cepheusservice.domain.entity.ProductEntity;
 import com.example.web.cepheusservice.domain.entity.UserEntity;
 import com.example.web.cepheusservice.mappers.Mapper;
+import com.example.web.cepheusservice.services.JwtService;
 import com.example.web.cepheusservice.services.UserServise;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +23,24 @@ public class UserController {
 
     private final Mapper<UserEntity, UserDto> mapper;
     private final UserServise userServise;
+    private final JwtService jwtService;
 
+    @PostMapping(path = "/users/{token}")
+    public ResponseEntity<UserDto> getUsers(@PathVariable String token) {
+        String userEmail = jwtService.extractUseremail(token);
+        UserEntity userEntity = userServise.findUserByEmail(userEmail);
+        UserDto userDto = mapper.mapTo(userEntity);
+
+//        UserEntity userEntity = mapper.mapFrom(userDto);
+//        System.out.println(userEntity);
+        return ResponseEntity.ok(userDto);
+    }
+
+//    Получаения всех пользователей
     @GetMapping(path = "/users")
-    public ResponseEntity<UserDto> getUsers(@RequestBody UserDto userDto) {
-        UserEntity userEntity = mapper.mapFrom(userDto);
-        System.out.println(userEntity);
-        return ResponseEntity.ok(mapper.mapTo(userEntity));
+    public List<UserDto> listUsers() {
+        List<UserEntity> userEntityList = userServise.findAll();
+        return userEntityList.stream().map(mapper::mapTo).collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/example/web/cepheusservice/services/UserServise.java
+++ src/main/java/com/example/web/cepheusservice/services/UserServise.java
@@ -1,4 +1,12 @@
 package com.example.web.cepheusservice.services;
 
+import com.example.web.cepheusservice.domain.entity.UserEntity;
+
+import java.util.List;
+
 public interface UserServise {
+
+    UserEntity findUserByEmail(String userEmail);
+
+    List<UserEntity> findAll();
 }
