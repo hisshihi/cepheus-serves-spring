e1f2c4878e33ec6837b897ee38277b52e922c55a
HissDev <hissoffc@gmail.com>
2024-02-12 16:42:18 +0300

    FIXED

--- src/main/java/com/example/web/cepheusservice/CepheusServiceApplication.java
+++ src/main/java/com/example/web/cepheusservice/CepheusServiceApplication.java
@@ -1,9 +1,7 @@
 package com.example.web.cepheusservice;
 
-import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class CepheusServiceApplication {
