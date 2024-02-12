744c818f50c149b3139806f457489bfc95df2b31
HissDev <hissoffc@gmail.com>
2024-02-12 15:58:50 +0300

    Fixed number 8

--- src/main/java/com/example/web/cepheusservice/config/CorsConfig.java
+++ src/main/java/com/example/web/cepheusservice/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://cepheus-serves-spring-production.up.railway.app/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowedHeaders("*");
             }
