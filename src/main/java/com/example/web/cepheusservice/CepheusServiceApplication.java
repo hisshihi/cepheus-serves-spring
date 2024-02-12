a3254ba5c80c85ccbe96ce5eef04a848685055ab
HissDev <hissoffc@gmail.com>
2024-02-12 15:41:06 +0300

    Fixed number 6

--- src/main/java/com/example/web/cepheusservice/CepheusServiceApplication.java
+++ src/main/java/com/example/web/cepheusservice/CepheusServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile("dev")
 public class CepheusServiceApplication {
 
 	public static void main(String[] args) {
