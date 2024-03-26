058b7cb1e0f5e3afb82e46b171072a0206993746
HissDev <hissoffc@gmail.com>
2024-03-26 23:28:06 +0300

    Вернул обратно к разработке на стороне тестирования и добавил endpoint для популярных товаров

--- src/main/java/com/example/web/cepheusservice/config/CorsConfig.java
+++ src/main/java/com/example/web/cepheusservice/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://master--cepheus-service.netlify.app")
+                        .allowedOrigins("http://localhost:8081")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowedHeaders("*");
             }
